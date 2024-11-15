561d736574efb878021aa7d02e937f093cc04ae4
Nuhamin Tibebu <nuhamintibebu965@gmail.com>
2024-11-15 11:02:01 +0300

    Initial commit

---
+++ JavaScript_Basics.js
@@ -0,0 +1,9 @@
+//Variables are a container for storing  data
+let firstName = "Nuhamin";
+let age = 21;
+let student = true;
+
+age = age + 1;
+console.log(firstName, "Tibebu");
+console.log("you are", age, "years old");
+console.log(student);
